68fe2ce5eefdd85510737e983315af406b20f30c
menezescarlos <menezesoliveira40@hotmail.com>
2020-07-24 00:04:41 -0300

    Exceções try{}catch{}

---
+++ bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/Conta.java
@@ -0,0 +1,5 @@
+package br.com.alura.heranca.modelo.depuracao;
+
+public class Conta {
+	void deposita() {}
+}
--- bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/Depuração.java
+++ bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/Depuração.java
@@ -3,20 +3,42 @@ package br.com.alura.heranca.modelo.depuracao;
 public class Depuração {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("ArithmeticException " + msg);
+			ex.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
+		// try {
 		metodo2();
+		/*
+		 * } catch (ArithmeticException e) { System.out.println("ArithmeticException");
+		 * }
+		 */
+
 		System.out.println("Fim do metodo1");
+
 	}
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+			Conta c = null;
+			c.deposita();
+			// try {
+			//int a = i / 0;
+			/*
+			 * } catch (ArithmeticException e) { System.out.println("ArithmeticException: "
+			 * + e); }
+			 */
+
 		}
 		System.out.println("Fim do metodo2");
 	}
