ec52b61280ac317057664595631c28b7362f48c8
menezescarlos <menezesoliveira40@hotmail.com>
2020-07-24 23:52:39 -0300

    throw exception

--- bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/FluxoComTratamento.java
+++ bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/FluxoComTratamento.java
@@ -1,6 +1,6 @@
 package br.com.alura.heranca.modelo.depuracao;
 
-public class Depuração {
+public class FluxoComTratamento {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try {
@@ -15,21 +15,31 @@ public class Depuração {
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
+		// try {
 		metodo2();
+		/*
+		 * } catch (ArithmeticException e) { System.out.println("ArithmeticException");
+		 * }
+		 */
+
 		System.out.println("Fim do metodo1");
 
 	}
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		/*ArithmeticException exception = new ArithmeticException();
-		throw exception;*/
-		
-		throw new ArithmeticException("DEU ERRO");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			Conta c = null;
+			c.deposita();
+			// try {
+			//int a = i / 0;
+			/*
+			 * } catch (ArithmeticException e) { System.out.println("ArithmeticException: "
+			 * + e); }
+			 */
 
-		
-		
-		
+		}
 		System.out.println("Fim do metodo2");
 	}
 }
