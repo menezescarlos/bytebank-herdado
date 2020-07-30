a61c8d0d62721bc3755711a5604abb43a2e7d3a4
menezescarlos <menezesoliveira40@hotmail.com>
2020-07-29 23:12:05 -0300

    Cast com arrey

--- bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/TesteArrayReferencias.java
+++ bytebank-herdado/src/br/com/alura/heranca/modelo/depuracao/TesteArrayReferencias.java
@@ -3,17 +3,30 @@ package br.com.alura.heranca.modelo.depuracao;
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
-		ContaCorrente[] contas = new ContaCorrente[5];
-		
+		/*
+		 * ContaCorrente[] contas = new ContaCorrente[5];
+		 * 
+		 * ContaCorrente c1 = new ContaCorrente(111, 222, 100.0); ContaCorrente c2 = new
+		 * ContaCorrente(222, 111, 300.0);
+		 * 
+		 * contas[0] = c1; contas[1] = c2;
+		 * 
+		 * ContaCorrente ref = contas[0]; System.out.println(ref.getNumero());
+		 * System.out.println(contas[1].getNumero());
+		 */
+
+		Conta[] contas = new Conta[5];
+
 		ContaCorrente c1 = new ContaCorrente(111, 222, 100.0);
 		ContaCorrente c2 = new ContaCorrente(222, 111, 300.0);
-		
+
 		contas[0] = c1;
 		contas[1] = c2;
-		
-		System.out.println(contas[0].getSaldo());
+
+		ContaCorrente ref = (ContaCorrente) contas[0];
+		System.out.println(ref.getNumero());
 		System.out.println(contas[1].getNumero());
-		
+
 	}
 
 }
